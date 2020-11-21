78d2b4b49c8e540c888e5d0338dfa715ac44aa52
mariosknl <mariosknl@outlook.com>
2020-11-21 19:18:19 +0200

    splitting mainbutton for each platform on its own component files

--- components/MainButton.android.js
+++ components/MainButton.android.js
@@ -13,7 +13,7 @@ import Colors from "../constants/colors";
 const MainButton = (props) => {
   let ButtonComponent = TouchableOpacity;
 
-  if (Platform.OS === "android" && Platform.Version >= 21) {
+  if (Platform.Version >= 21) {
     ButtonComponent = TouchableNativeFeedback;
   }
 
