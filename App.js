c3f5a4cb3ff9fa4037a346881ae4b8622db5b715
mariosknl <mariosknl@outlook.com>
2020-11-21 19:24:31 +0200

    safeareaview implemented

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from "expo-status-bar";
 import React, { useState } from "react";
-import { StyleSheet, View } from "react-native";
+import { StyleSheet, View, SafeAreaView } from "react-native";
 import * as Font from "expo-font";
 import { AppLoading } from "expo";
 
@@ -61,11 +61,11 @@ export default function App() {
   }
 
   return (
-    <View style={styles.screen}>
+    <SafeAreaView style={styles.screen}>
       <Header title="Guess A Number" />
       {content}
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
